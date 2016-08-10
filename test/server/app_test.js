b5c332043213eaf57bc56c00b24c51728a6f3041
Tyler Green <tyler@Tylers-MacBook-Air.local>
2016-08-10 12:05:47 -0500

    Add failing test for login

--- test/server/app_test.js
+++ test/server/app_test.js
@@ -31,8 +31,29 @@ describe('Post ', () =>{
                         user_response.hasOwnProperty('lastName'))
             })
             .end(done)
-    })
+    })})
 
+describe('Post ', () =>{
+    //need to have an exising user
+    var user = JSON.stringify({ email: "dude@gmail.com",
+                                password: "abcde"})
 
+    it('logs into an account', (done) => {
+        // user should already exist bc there is currently no setup or teardown
+        api.post('/login')
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .send(user)
+            .expect(200)
+            .expect((res) => {
+                login_response = res.body.user // not sure this is right either
+                return ((login_response.hasOwnProperty('user') &&
+                         login_response.hasOwnProperty('token')))
+            })
+            .end(done)
+    }
+      ) 
 
+        
+})
 
