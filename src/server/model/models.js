102111e92fffeddd413fbe4ac636286f4b27d733
Tyler Green <tyler@Tylers-MacBook-Air.local>
2016-08-22 14:37:46 -0500

    lowercase file name in require

--- src/server/model/models.js
+++ src/server/model/models.js
@@ -1,7 +1,7 @@
-var UserMeta = require('./User.js'),
+var UserMeta = require('./user.js'),
     connection = require('../sequelize.js')
 
-var User = connection.define('users', UserMeta.attributes, UserMeta.options)
+var User = connection.define('Users', UserMeta.attributes, UserMeta.options)
 
 // you can define relationships here
 
